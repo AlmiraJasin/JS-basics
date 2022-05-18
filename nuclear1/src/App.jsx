1cf317fd0e822cf7c5e9e9c6869cd1ac64eb08bd
Almira Jasinskyte <101142015+AlmiraJasin@users.noreply.github.com>
2022-05-18 13:46:14 +0300

    add new react project

---
+++ nuclear1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './Components/009/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Hello></Hello>
+      </header>
+    </div>
+  );
+}
+
+export default App;
