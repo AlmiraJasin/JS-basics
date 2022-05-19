240dcf49114d6f43acc0a1b78977284883886996
Almira Jasinskyte <101142015+AlmiraJasin@users.noreply.github.com>
2022-05-19 13:42:34 +0300

    States

---
+++ nuclear1/src/009.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+
+  const mygtukas = () => {
+    console.log('As grazus mygtukas');
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>State</h1>
+        <button>Press me!</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- nuclear1/src/App.jsx
+++ nuclear1/src/App.jsx
@@ -1,16 +1,45 @@
+import { useState } from 'react';
 import './App.css';
-import Hello from './Components/009/Hello';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Hello spalva="green" skaicius={4}></Hello>
-        <Hello spalva="black" skaicius={4}></Hello>
-        <Hello spalva="red" skaicius={4}></Hello>
-      </header>
-    </div>
-  );
+    const [spalva, setSpalva] = useState('yellow');
+    /* const mygtukas = () => {
+        console.log('As grazus mygtukas');
+        return mygtukoBrolis;
+    }
+    const mygtukoBrolis = () => {
+        console.log('As grazus mygtuko brolis');
+    } */
+    const beArgumentu = () => {
+        console.log('beArgumentu');
+    }
+    const suArgumentu = ka => {
+        console.log('suArgumentu' + ka);
+    }
+    const keistiSpalva = () => {
+        // const naujaSpalva = spalva === 'yellow' ? 'pink' : 'yellow'; - neteisingas budas
+        // teisingas budas:
+        setSpalva(old => old === 'yellow' ? 'pink' : 'yellow');
+        // console.log(spalva);
+    }
+
+    const [skaicius, setSkaicius] = useState(1);
+
+    const skaiciuotuvas = (reiksme) => {
+        setSkaicius(skaiciukas => skaiciukas + reiksme);
+    }
+    return (
+        <div className="App">
+        <header className="App-header">
+            <h1 style={{color: spalva}}>State {skaicius}</h1>
+            <button onClick={() => skaiciuotuvas(1)}>Add One</button>
+            <button onClick={() => skaiciuotuvas(-1)}>Subs One</button>
+            <button onClick={() => suArgumentu(' Labas')}>Press me! SU</button>
+            <button onClick={beArgumentu}>Press me! BE</button>
+            <button onClick={keistiSpalva}>Kita Spalva</button>
+        </header>
+        </div>
+    );
 }
 
 export default App;
