7fe875608cf3a1a5e5c6472c450bac58159d1d94
Almira <jasinskyte.almira@gmail.com>
2022-05-26 13:11:28 +0300

    pask

--- nuclear1/src/App.jsx
+++ nuclear1/src/App.jsx
@@ -1,72 +1,23 @@
-import { useEffect, useRef, useState } from 'react';
+//import { useState } from 'react';
+import { useState } from 'react';
 import './App.scss';
-import rand from './Functions/rand';
+//import randColor from './Functions/randColor';
+//import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
-
-    const [kv, setKv] = useState(null);
-    const istorija = useRef([]);
-
-    // PIRMAS KROVIMAS
-    useEffect(() => {
-        setKv(JSON.parse(localStorage.getItem([]))); // gali buti null arba []
-    }, []);
-
-    // UZSAUGOS POKYCIUS
-    useEffect(() => {
-        if (null === kv) {
-            return;
-        }
-        localStorage.setItem('kv', JSON.stringify(kv));
-        istorija.current.unshift(kv);
-    }, [kv]);
-
-    const prideti = () => {
-        const kiekis = rand(5, 10);
-        const kvadratukai = [];
-        for (let i = 0; i < kiekis; i++) {
-            kvadratukai.push('^o^');
-        }
-        setKv(k => null === k ? [...kvadratukai] : [...k, ...kvadratukai]);
-    }
-
-    const isvalyti = () => {
-        setKv([]);
-    }
-
-    const atgal = () => {
-        let senas = istorija.current.shift();
-        if (!senas) {
-            setKv([]);
-        } else if (senas.length === kv.length) {
-            senas = istorija.current.shift();
-            if (!senas) {
-                setKv([]);
-            } else {
-                setKv(senas);
-            }
-        }
-        else {
-            setKv(senas);
-        }
+    const [text, setText] = useState('');
+    const inputText = e => {
+        setText(e.target.value);
     }
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>PRAKTIMUMAS</h1>
-                    <div className="kvc">
-                        {
-                            kv ? kv.map((k, i) => <div key={i} className="kv">{k}</div>) : null
-                        }
-                    </div>
-
-                <button onClick={prideti}>Pridėti</button>
-                <button onClick={isvalyti}>Išvalyti</button>
-                <button onClick={atgal}>Atgal</button>
-            </header>
-        </div>
+      <div className="App">
+        <header className="App-header">
+            <input type="text" onChange={inputText} value={text}></input>
+        </header>
+      </div>
     );
-}
-
-export default App;
+  }
+  
+  export default App;
